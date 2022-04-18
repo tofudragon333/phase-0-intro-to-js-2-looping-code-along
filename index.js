4723317431613d2c0d9a2fade8bcde35b8d1a6c6
Ashley Wong <tofudragon333@gmail.com>
2022-04-17 20:36:14 -0400

    commit tree 9fb0fbbd7ce9b2d512ab68723cade9e4137d2140

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(name, event) {
+    const messages = [];
+    for (let i = 0; i < name.length; i++) {
+        messages[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    
+    }
+
+    return messages;
+}
+
+const number = [];
+
+function countDown(number) {
+    let x = 10;
+while (x >= 0) {
+    console.log(x);
+    x--;
+}
+return number;
+}
